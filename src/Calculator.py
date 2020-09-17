743fdf591a6d19f2b2a7b8c8bb7ed90b05ced917
Diego Figueroa Llamosas <35986201+DiegoAnFi@users.noreply.github.com>
2020-09-17 16:00:28 -0500

    Improve logic

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,25 +1,33 @@
 print("Welcome to PyCalculator!")
-print("To close calculator type 'close'")
 num1 = int(input("num1: "))
 op = input("Operator: ")
 num2 = int(input("num2: "))
 
 while True:
-
     if op == "+":
-        print(num1 + num2)
+        num3 = num1 + num2
     elif op == "-":
-        print(num1 - num2)
+        num3 = num1 - num2
     elif op == "/":
-        print(num1 / num2)
+        num3 = num1 / num2
     elif op == "*":
-        print(num1 * num2)
+        num3 = num1 * num2
+    elif op == "reset":
+        continue
     elif op == "close":
         break
     else:
         print("Invalid Operator")
+        op = input("Operator: ")
+        continue
 
+    print(num1,op,num2,'=',num3)
+    num1 = num3
+    print('num1:', num1)
     op = input("Operator: ")
+    if op == "close":
+        break
+    num2 = int(input("num2: "))
 
 
     
